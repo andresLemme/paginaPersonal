07b7a6a335f9a9a25d29d73fd942822e46782bd1
Pocas1982 <andres.lemme@gmail.com>
2021-07-24 13:17:12 -0300

    all component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Component/Header';
+import Main from './Component/Main';
+import About from './Component/About';
+import Skill from './Component/Skill';
+import Projects from './Component/Projects';
+import Contact from './Component/Contact';
+import Footer from './Component/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+   <Header/>
+   <Main/>
+   <About/>
+   <Skill/>
+   <Projects/>
+   <Contact/>
+   <Footer/>
+  </>
+  )
 }
 
 export default App;
---
+++ src/Component/Skill/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Skill(){
+  return(
+    <>
+    <p>Soy mis ocnocimientos</p>
+    </>
+  )
+}
