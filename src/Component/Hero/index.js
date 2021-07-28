adb0940e887bec344fea7476526ccbadfcfb2c00
Pocas1982 <andres.lemme@gmail.com>
2021-07-28 20:09:20 -0300

    add hero section

--- src/Component/Hero/index.js
+++ src/Component/Hero/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './hero.scss'
 
 export default function Hero(){
   return(
-    <section>
-      dfsdfsdfsdf
+    <section className="container-main">
+      <div className="block-division">
+        <div className="block-left">
+        <h3>Hola,</h3>
+          <h1>Soy Andrés Federico Lemme</h1>
+          <p>Lic en Diseño Gráfico <br/> Tester Analyst and Frontend Dev</p>
+
+        </div>
+        <div className="block-right">
+        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF-raMfGj5OqA/profile-displayphoto-shrink_800_800/0/1518585170510?e=1632960000&v=beta&t=J0hAAKWLrunPnfmmVNvulbr1e8v-CgYzpUumXh7pFbg" alt="Andres Lemme" />
+        </div>
+      </div>
     </section>
   )
 }
